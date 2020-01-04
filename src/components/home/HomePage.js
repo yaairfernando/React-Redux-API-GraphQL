a9a2c2df16a67942598a5fd2672d1e118829e9af
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-01-04 09:30:27 -0600

    Using connect function to get the characters from the redux store

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,38 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Card from '../card/Card'
 import styles from './home.module.css'
-import axios from 'axios'
+import { connect } from 'react-redux';
 
-let URL = "https://rickandmortyapi.com/api"
-
-export default function Home() {
-
-    let [chars, setChars] = useState([])
-
-    useEffect(() => {
-        getCharacters()
-    }, [])
-
-    function nextChar() {
-        chars.shift()
-        if (!chars.length) {
-            //get more characters
-        }
-        setChars([...chars])
-    }
-
+function Home({chars}) {
     function renderCharacter() {
         let char = chars[0]
         return (
-            <Card leftClick={nextChar} {...char} />
+            <Card {...char}/>
         )
-    }
-
-    function getCharacters() {
-        return axios.get(`${URL}/character`)
-            .then(res => {
-                setChars(res.data.results)
-            })
     }
 
     return (
@@ -44,3 +20,9 @@ export default function Home() {
         </div>
     )
 }
+
+const mapStateToProps = (state) => {
+  return { chars: state.characters.array }
+}
+
+export default connect(mapStateToProps)(Home);
