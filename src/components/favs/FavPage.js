95ad5fd77c24e501002132a9075b05f985878740
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-01-04 12:04:06 -0600

    Modify favorites component

--- src/components/favs/FavPage.js
+++ src/components/favs/FavPage.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import styles from './favs.module.css'
 import Card from '../card/Card'
+import { connect } from 'react-redux';
 
-export default function FavPage({ characters = [0] }) {
+function FavPage({ favorites = [0] }) {
     function renderCharacter(char, i) {
         return (
-            <Card key={i} />
+            <Card hide {...char} key={i} />
         )
     }
     return (
         <div className={styles.container}>
-            <h2>Favoritos</h2>
-            {characters.map(renderCharacter)}
-            {!characters.length && <h3>No hay personajes agregados</h3>}
+            <h2>Favorites</h2>
+            {favorites.map(renderCharacter)}
+            {!favorites.length && <h3>There are not any favorites...</h3>}
         </div>
     )
 }
+
+const matStateToProps = ({characters:{favorites}}) => {
+  return {
+    favorites
+  }
+}
+
+export default connect(matStateToProps)(FavPage);
