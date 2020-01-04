8e22e4c44b9671dad1674ef86ada27e832c67250
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-01-04 09:18:46 -0600

    Adding the success and error case

--- src/components/redux/charsDuck.js
+++ src/components/redux/charsDuck.js
@@ -13,20 +13,32 @@ const GET_CHARACTERS_ERROR = "GET_CHARACTERS_ERROR"
 export default function reducer(state= INITIALDATA, action) {
   switch(action.type) {
     case GET_CHARACTERS:
+      return { ...state, fetching: true }
     case GET_CHARACTERS_ERROR:
+      return { ...state, fetching: false, error: action.payload }
     case GET_CHARACTERS_SUCCESS:
-      return { ...state, array: action.payload }
+      return { ...state, array: action.payload, fetching: false }
     default:
       return state;
   }
 }
 // Action Creator
 export let getCharactersAction = ()=> (dispatch, getState) => {
+  dispatch({
+    type: GET_CHARACTERS
+  })
   return axios.get(URL)
     .then(res => {
       dispatch({
         type: GET_CHARACTERS_SUCCESS,
         payload: res.data.results
       });
-    });
+    })
+    .catch(err => {
+      console.log(err)
+      dispatch({
+        type: GET_CHARACTERS_ERROR,
+        payload: err.response.message
+      })
+    })
 };
